37a7f638a21b6923936c0ceb28502e9da110dfea
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2025-02-19 01:14:50 +0300

    add unit tests

---
+++ src/main/java/ulasevich/CargoDimension.java
@@ -0,0 +1,16 @@
+package ulasevich;
+
+public enum CargoDimension {
+    LARGE(200),
+    SMALL(100);
+
+    private int costIncrease;
+
+    CargoDimension(int costIncrease) {
+        this.costIncrease = costIncrease;
+    }
+
+    public int getCostIncrease() {
+        return costIncrease;
+    }
+}
