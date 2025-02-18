411b0748d65dcc855a7a89d88fe9aec6e4753da2
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2025-02-19 01:15:08 +0300

    refactor

--- src/test/java/DeliveryCostCalculationTest.java
+++ src/test/java/DeliveryCostCalculationTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import ulasevich.CargoDimension;
 import ulasevich.Delivery;
 import ulasevich.ServiceWorkload;
